0149536db3513087213d570fd7752d013f7d5357
manojvicky <manoj.save.earth@gmail.com>
2019-04-17 01:39:50 +0530

    modal changes

--- ui/src/components/AddTodo/AddTodo.js
+++ ui/src/components/AddTodo/AddTodo.js
@@ -11,41 +11,7 @@ constructor(){
 render(){
     return (
       <div className="addtodo">
-      <div className="container">
-        <div className="row">
-          <div className="Col" lg={6} md={6} sm={12} xs={12} style={{marginBottom:"5px"}}>
-            <div className="row">
-              <div className="Col" lg={4} md={5} sm={12} xs={12}>
-                <label className="titlediv">Title :</label>
-              </div>
-              <div className="Col" lg={8} md={7} sm={12} xs={12}>
-                <InputElement stateProps={this.state.stateProps}/>
-              </div>
-            </div>
-          </div>
-          <div className="Col" lg={6} md={6} sm={12} xs={12} style={{marginBottom:"5px"}}>
-            <div className="row">
-              <div className="Col" lg={4} md={5} sm={12} xs={12}>
-                <label className="titlediv">Description :</label>
-              </div>
-              <div className="Col" lg={8} md={7} sm={12} xs={12}>
-                <InputElement stateProps={this.state.stateProps}/>
-              </div>
-            </div>
-          </div>
-          <div className="Col" lg={{span: 4, offset: 4}} md={{span: 4, offset: 4}} sm={{span: 6, offset: 3}} xs={12}>
-          <div
-            className="addtodobutton"
-            onClick={() => {
-              this.setState({stateProps: "YooooY"});
-              // this.props.action({ title: this.state.title, description: "yoo i am the best" });
-            }}
-          >
-            Add Todo
-          </div>
-          </div>
-        </div>
-       </div>
+      
       </div>
     );
 }
--- ui/src/components/Todo/Todo.js
+++ ui/src/components/Todo/Todo.js
@@ -1,15 +1,10 @@
 import React from "react";
 import "../../styles.css";
-import TodoList from "../TodoList/TodoList.container";
-import AddTodo from "../AddTodo/AddTodo.container";
-import AddTodoIcon from "../AddTodo/AddTodoIcon";
-
-import Header from "../Header/Header"
+import {TodoList, AddTodoIcon, Header} from "../";
 const Todo = () => {
   return (
     <div className="todo">
       <Header/>
-      <AddTodo />
       <AddTodoIcon/>
       <TodoList />
     </div>
